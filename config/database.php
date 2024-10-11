f98e688af05b32dcbc7137b89b58142dda8d0b38
Rubem Neto <rubemviscard2635@gmail.com>
2024-10-11 19:47:07 +0100

    Change Redis Service Environment Vars

--- config/database.php
+++ config/database.php
@@ -130,19 +130,19 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_SERVICE_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => env('REDIS_SERVICE_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_SERVICE_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
+            'port' => env('REDIS_SERVICE_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
